544b816920974f4f6fe3ac8f99d9640c390e9a5a
malloryklar0516 <malloryklar@Mallorys-MacBook-Air.local>
2024-05-06 15:03:16 -0400

    deploy fix

--- server.js
+++ server.js
@@ -40,6 +40,6 @@ const sess = {
   
   app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
-    sequelize.sync({ force: true });
+    sequelize.sync({ force: false });
   });
   
